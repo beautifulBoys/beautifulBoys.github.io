5ed0719e60360fe3e0a11fb841874f075a91d7af
beautifulBoys <1298947916@qq.com>
2017-03-10 15:49:02 +0800

    1

---
+++ html/home/data.js
@@ -0,0 +1,230 @@
+/**
+ * Created by user on 2017/3/10.
+ */
+
+var data = {
+  "menuList": [
+    {
+      "id": "0",
+      "text": "PC端网站",
+      "english": "PC pages",
+      "desc": "这是一些业余时间做的PC端网站",
+      "items": [
+        {
+          "title": "唯一视觉",
+          "time": "2016-03-21",
+          "href": "https://beautifulboys.github.io/html/weiyi/",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a1.jpg",
+          "site": {
+            "official": "http://www.onlyphoto.com/",
+            "model": "https://beautifulboys.github.io/html/weiyi/"
+          },
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "title": "小米网",
+          "time": "2016-12-08",
+          "href": "https://beautifulboys.github.io/html/xiaomi/",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a2.jpg",
+          "site": {
+            "official": "http://www.mi.com/",
+            "model": "https://beautifulboys.github.io/html/xiaomi/"
+          },
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "title": "豆瓣电影",
+          "time": "2016-06-15",
+          "href": "https://beautifulboys.github.io/html/douban/",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a3.jpg",
+          "site": {
+            "official": "https://movie.douban.com/",
+            "model": "https://beautifulboys.github.io/html/douban/"
+          },
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "title": "天猫登录",
+          "time": "2016-07-03",
+          "href": "https://beautifulboys.github.io/html/tmall/",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a4.jpg",
+          "site": {
+            "official": "http://login.tmall.com",
+            "model": "https://beautifulboys.github.io/html/tmall/"
+          },
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "title": "沈阳化工大学",
+          "time": "2016-02-19",
+          "href": "https://beautifulboys.github.io/html/huada/",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a11.jpg",
+          "site": {
+            "official": "",
+            "model": "https://beautifulboys.github.io/html/huada/"
+          },
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        }
+      ]
+    },
+    {
+      "id": "1",
+      "text": "移动端网站",
+      "english": "Mobile pages",
+      "desc": "移动端流式布局网站",
+      "items": [
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a5.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/app.png",
+          "title": "APP随想",
+          "time": "2017-03-05",
+          "content": "App随想是本人在业余学习过程中的积累，内容也许是一个好看的网页，也许是为了研究某种特效或者技术而随意而为。"
+        },
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a6.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/mi.png",
+          "title": "小米网移动",
+          "time": "2017-02-12",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a7.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/bantang.png",
+          "title": "半糖App",
+          "time": "2016-09-20",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a8.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/baifubao.png",
+          "title": "百付宝",
+          "time": "2015-10-10",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        }
+      ]
+    },
+    {
+      "id": "2",
+      "text": "Canvas运用",
+      "english": "Canvas",
+      "desc": "Canvas做的一些动画",
+      "items": [
+        {
+          "href": "https://beautifulboys.github.io/html/canvas/time/",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a9.jpg",
+          "title": "酷炫时间",
+          "time": "2017-02-20",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a12.jpg",
+          "title": "图片放大",
+          "time": "2017-02-16",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        }
+      ]
+    },
+    {
+      "id": "3",
+      "text": "小例子",
+      "english": "Example",
+      "desc": "业余做的一些小例子",
+      "items": [
+        {
+          "href": "https://beautifulboys.github.io/html/example/bullet/",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a13.jpg",
+          "title": "弹幕系统",
+          "time": "2016-10-10",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "https://beautifulboys.github.io/html/example/taobao/",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a14.jpg",
+          "title": "淘宝大图浏览",
+          "time": "2016-03-26",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a15.jpg",
+          "title": "在线视频播放器",
+          "time": "2016-11-03",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        }
+      ]
+    },
+    {
+      "id": "4",
+      "text": "学习笔记",
+      "english": "Study notes",
+      "desc": "前端学习笔记",
+      "items": [
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a1.jpg",
+          "title": "国务委员王勇视察中农发集团中牧研究院",
+          "time": "2016-02-23",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a2.jpg",
+          "title": "国务委员王勇视察中农发集团中牧研究院",
+          "time": "2016-02-23",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a3.jpg",
+          "title": "国务委员王勇视察中农发集团中牧研究院",
+          "time": "2016-02-23",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a4.jpg",
+          "title": "国务委员王勇视察中农发集团中牧研究院",
+          "time": "2016-02-23",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        }
+      ]
+    },
+    {
+      "id": "5",
+      "text": "收藏夹",
+      "english": "Favorites",
+      "desc": "学习资料收藏夹",
+      "items": [
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a5.jpg",
+          "title": "国务委员王勇视察中农发集团中牧研究院",
+          "time": "2016-02-23",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a6.jpg",
+          "title": "国务委员王勇视察中农发集团中牧研究院",
+          "time": "2016-02-23",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a7.jpg",
+          "title": "国务委员王勇视察中农发集团中牧研究院",
+          "time": "2016-02-23",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        },
+        {
+          "href": "http://www.lixin5.top",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a8.jpg",
+          "title": "国务委员王勇视察中农发集团中牧研究院",
+          "time": "2016-02-23",
+          "content": "中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂价数据来看，三季度经中国经济三季报本周将出炉。从已经公布的外贸、物价和工业品出厂"
+        }
+      ]
+    }
+  ]
+};
