4b233cd217f49e5b2f048dab1dfb13cee7536482
beautifulBoys <1298947916@qq.com>
2017-03-22 15:00:45 +0800

    1

---
+++ html/blog1/data.js
@@ -0,0 +1,71 @@
+/**
+ * Created by user on 2017/3/22.
+ */
+
+
+var data = {
+    "title": "JavaScript的函数柯里化与教程详解",
+    "content": [
+        {
+            "type": "p",
+            "text": `我们已经生成了我们的第一个 Vue 应用！看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。我们如何知道？打开你的浏览器的控制台，并修改 app.message，看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。我们如何知道？打开你的浏览器的控制台，并修改 app.message，看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。我们如何知道？打开你的浏览器的控制台，并修改 app.message，我们如何知道？打开你的浏览器的控制台，并修改 app.message，你将看到上例相应地更新。`
+        },
+        {
+            "type": "h",
+            "text": `<div class='header'>
+  <div class='box1'>
+    <div class='logo'></div>
+  </div>
+</div>`
+        },
+        {
+            "type": "p",
+            "text": `我们已经生成了我们的第一个 Vue 应用！看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。我们如何知道？打开你的浏览器的控制台，并修改 app.message，你将看到上例相应地更新。`
+        },
+        {
+            "type": "p",
+            "text": `我们已经生成了我们的第一个 Vue 应用！看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。我们如何知道？打开你的浏览器的控制台，并修改 app.message，我们如何知道？打开你的浏览器的控制台，并修改 app.message，你将看到上例相应地更新。`
+        },
+        {
+            "type": "j",
+            "text": `function Stack() {
+  this.length = function () {
+    return this.top;
+  };
+  this.top = 0;
+  this.stackStore = [];
+  this.push = function (str) {
+    this.stackStore[this.top++] = str;
+  };
+}`
+        },
+        {
+            "type": "p",
+            "text": `我们已经生成了我们的第一个 Vue 应用！看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。我们如何知道？打开你的浏览器的控制台，并修改 app.message，看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。我们如何知道？打开你的浏览器的控制台，并修改 app.message，看起来这跟单单渲染一个字符串模板非常类似，但是 Vue.js 在背后做了大量工作。现在数据和 DOM
+      已经被绑定在一起，所有的元素都是响应式的。我们如何知道？打开你的浏览器的控制台，并修改 app.message，你将看到上例相应地更新。`
+        },
+        {
+            "type": "c",
+            "text": `pre {
+  width: 100%;
+  padding: 0 10px;
+  background: #272822;
+  box-sizing: border-box;
+  color: #fff;
+  border-radius: 5px;
+  line-height: 16px;
+  margin-bottom: 0;
+  font-size: 0;
+}`
+        }
+    ]
+};
