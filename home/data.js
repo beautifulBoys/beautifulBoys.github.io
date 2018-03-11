baaa9ead97472d85e7f97dc80eedda073b340588
beautifulBoys <1298947916@qq.com>
2018-03-11 13:41:01 +0800

    调整目录

---
+++ home/data.js
@@ -0,0 +1,284 @@
+/**
+ * Created by user on 2017/3/10.
+ */
+
+var data = {
+  "menuList": [
+    {
+      "id": "0",
+      "text": "PC端网站",
+      "english": "PC pages",
+      "desc": "这是一些业余时间做的PC端网站",
+      "items": [
+        {
+          "title": "旅游圈",
+          "time": "2017-12-10",
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a18.jpg",
+          "site": {
+            "official": "",
+            "model": "../pc/tourism/web/index.html"
+          },
+          "content": "这个项目是一个包括PC端，移动端，小程序，后期还会实现安卓和IOS。算是一个学习的完整项目，该项目完全由自己规划，设计，编写，测试，部署完成的中性项目，收获良多。"
+        },
+        {
+          "title": "AIM 愛聊",
+          "time": "2017-10-16",
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a17.jpg",
+          "site": {
+            "official": "",
+            "model": "../pc/vue-socket/index.html"
+          },
+          "content": "这个项目是完全仿写腾讯 TIM 的 web 应用。通过该应用学习了聊天室的搭建，一对一私聊的实现等。"
+        },
+        {
+          "title": "后台管理系统",
+          "time": "2017-04-16",
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a16.jpg",
+          "site": {
+            "official": "",
+            "model": "../pc/responsive/index.html"
+          },
+          "content": "这个项目是用Bootstrap做的一个响应式的后台管理系统。用了element-ui, echarts, baiduMap等，项目还在不定期完善和填充内容。"
+        },
+        {
+          "title": "唯一视觉",
+          "time": "2016-03-21",
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a1.jpg",
+          "site": {
+            "official": "http://www.onlyphoto.net.cn/",
+            "model": "../pc/weiyi/index.html"
+          },
+          "content": "这个网站是在大四时课余时间写的，算是正式工作之前写的最用心的一个网页了，当时彻底是被他的页面设计和页面效果给迷住了。"
+        },
+        {
+          "title": "小米网",
+          "time": "2016-12-08",
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a2.jpg",
+          "site": {
+            "official": "http://www.mi.com/",
+            "model": "../pc/xiaomi/index.html"
+          },
+          "content": "不知不觉间，前前后后也用了三部小米手机和一个平板了，也渐渐从路人进化成了一个米粉，喜欢小米。"
+        },
+        {
+          "title": "豆瓣电影",
+          "time": "2016-06-15",
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a3.jpg",
+          "site": {
+            "official": "https://movie.douban.com/",
+            "model": "../pc/douban/index.html"
+          },
+          "content": "一直都听说：豆瓣的网站很有设计感，无论是配色、排版还是风格都很别致，是用心设计过的优秀作品。在此特意模仿一个，以提高自身的审美和编码能力。用PHP写的简单后台接口，提供Ajax请求。（在此处php已剔除，模拟的假请求）"
+        },
+        {
+          "title": "天猫登录",
+          "time": "2016-07-03",
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a4.jpg",
+          "site": {
+            "official": "http://login.tmall.com",
+            "model": "../pc/tmall/index.html"
+          },
+          "content": "偶然发现天猫登录页的登录块是用iframe嵌套的页面，与天猫全站弹出登录是共用的一个页面。很好奇这种方式，为此特意模仿天猫登录页面。相似度95%以上。"
+        },
+        {
+          "title": "沈阳化工大学",
+          "time": "2016-02-19",
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a11.jpg",
+          "site": {
+            "official": "http://www.syuct.edu.cn/",
+            "model": "../pc/huada/index.html"
+          },
+          "content": "由于学校的官网年久失修，板式老套，打算给做个改版，后来未能完成，实为憾事 ^ _ ^。采用php编写后台代码，在此PHP已剔除，只留下了html做展示，共计6个页面。"
+        }
+      ]
+    },
+    {
+      "id": "1",
+      "text": "移动端网站",
+      "english": "Mobile pages",
+      "desc": "移动端流式布局网站",
+      "items": [
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a19.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/responsive.png",
+          "title": "后台管理系统",
+          "time": "2017-04-18",
+          "content": "为上方PC端网站增加一个手机浏览入口。"
+        },
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a13.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/question.png",
+          "title": "答题系统",
+          "time": "2017-03-28",
+          "content": "该答题系统将答题流程程序化，可以很方便的将题目呈现给别人。前端展示来源于驾校一点通小程序，用VUE做数据驱动，未借助其他工具库。目前只支持单选，以后会陆续实现多功能化。"
+        },
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a5.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/app.png",
+          "title": "APP随想",
+          "time": "2017-03-05",
+          "content": "App随想是本人在业余学习过程中的积累，内容繁杂，也许是一个好看的网页，也许是为了研究某种特效或技术而随意而为。"
+        },
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a6.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/mi.png",
+          "title": "小米网移动",
+          "time": "2017-02-12",
+          "content": "本站是小米web官网的仿版。小米官网通体设计为白色背景，显得干净整洁；内容多，却不显杂乱。看到喜欢的页面，就忍不住想要模仿一个…… 本站采用vue整套技术栈。"
+        },
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a7.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/bantang.png",
+          "title": "半糖App",
+          "time": "2016-09-20",
+          "content": "本项目是模仿半糖App写的。"
+        },
+        {
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a8.jpg",
+          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/baifubao.png",
+          "title": "百付宝",
+          "time": "2015-07-10",
+          "content": "此项目是根据百度钱包手机APP仿写。包含6个页面：个人中心，账单，充值，亲密付，礼券，礼券单页。"
+        }
+      ]
+    },
+    {
+      "id": "2",
+      "text": "Canvas运用",
+      "english": "Canvas",
+      "desc": "Canvas做的一些动画",
+      "items": [
+        {
+          "href": "../canvas/time/index.html",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a9.jpg",
+          "title": "酷炫时间",
+          "time": "2017-02-20",
+          "content": "一个本地时间显示的小demo，特点是加了些canvas动画，时间改变的时候，五彩斑斓的小球在页面上坠落弹跳。。。"
+        }
+      ]
+    },
+    {
+      "id": "3",
+      "text": "小例子",
+      "english": "Example",
+      "desc": "业余做的一些小例子",
+      "items": [
+        {
+          "href": "../example/bullet/index.html",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a13.jpg",
+          "title": "弹幕系统",
+          "time": "2016-10-10",
+          "content": "该弹幕系统是完全模仿哔哩哔哩弹幕网制作。前端用vue，bootstrap，jquery完成功能。通信采用socket.io，后端用NodeJS完成，数据库采用MongoDB。"
+        },
+        {
+          "href": "../example/taobao/index.html",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a14.jpg",
+          "title": "淘宝大图浏览",
+          "time": "2016-03-26",
+          "content": "这是模仿淘宝网商品详情页大图浏览的功能。"
+        },
+        {
+          "href": "../example/resume/index.html",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a15.jpg",
+          "title": "个人简历",
+          "time": "2016-11-26",
+          "content": "个人简历。"
+        }
+      ]
+    },
+    {
+      "id": "4",
+      "text": "学习笔记",
+      "english": "Study notes",
+      "desc": "前端学习笔记",
+      "items": [
+        {
+          "href": "../blog/note/index.html",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a1.jpg",
+          "title": "JavaScript事件委托",
+          "time": "2017-01-19",
+          "content": "使用事件委托技术能让你避免对特定的每个节点添加事件监听器；相反，事件监听器是被添加到它们的父元素上。事件监听器会分析从子元素冒泡上来的事件，找到是哪个子元素的事件。基本概念非常简单，但仍有很多人不理解事件委托的工作原理。这里我将要解释事件委托是如何工作的，并提供几个纯JavaScript的基本事件委托的例子。"
+        },
+        {
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a2.jpg",
+          "title": "待填充",
+          "time": "2016-01-01",
+          "content": "待填充"
+        },
+        {
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a3.jpg",
+          "title": "待填充",
+          "time": "2016-01-01",
+          "content": "待填充"
+        },
+        {
+          "href": "",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a4.jpg",
+          "title": "待填充",
+          "time": "2016-01-01",
+          "content": "待填充"
+        }
+      ]
+    },
+    {
+      "id": "5",
+      "text": "收藏夹",
+      "english": "Favorites",
+      "desc": "学习资料收藏夹",
+      "items": [
+        {
+          "href": "http://www.cnblogs.com/dushao/p/6004883.html",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a5.jpg",
+          "title": "JavaScript十大经典算法",
+          "time": "2017-01-01",
+          "content": "JavaScript最经典的十种排序算法：冒泡排序，选择排序，插入排序，希尔排序，快速排序，归并排序，堆排序，计数排序，桶排序，基数排序"
+        },
+        {
+          "href": "http://www.codeceo.com/article/javascript-closure-interview.html",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a6.jpg",
+          "title": "JavaScript经典闭包题",
+          "time": "2017-01-01",
+          "content": "这是一道非常典型的JS闭包问题。其中嵌套了三层fun函数，搞清楚每层fun的函数是那个fun函数尤为重要。"
+        },
+        {
+          "href": "http://blog.csdn.net/jeffhtlee/article/details/6830458",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a7.jpg",
+          "title": " Javascript的原型继承是如何工作的",
+          "time": "2017-01-01",
+          "content": "到处可以看到Javascript是原型继承，但Javascript只提供了使用new关键字来实现原型继承，所以很多文章读后有诸多的疑问。这篇文章目的就是要说明Javascript的原型继承是如何工作的。"
+        },
+        {
+          "href": "https://www.talkingcoder.com/article/6404070502094668332",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a8.jpg",
+          "title": "JavaScript实现继承的方式",
+          "time": "2017-01-01",
+          "content": "主要介绍JavaScript实现继承的方式：类式继承、构造函数继承、组合继承、寄生组合式继承、extends继承"
+        },
+        {
+          "href": "http://ghmagical.com/article/page/id/UPLfoGI9vJ91",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a12.jpg",
+          "title": "JS中的call、apply、bind方法",
+          "time": "2017-01-01",
+          "content": "在JavaScript中，call、apply和bind 是Function对象自带的三个方法，这三个方法的主要作用是改变函数中的this指向。"
+        },
+        {
+          "href": "http://ghmagical.com/article/page/id/N7VY7Hg4TlgW",
+          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a13.jpg",
+          "title": "浅析JS模块规范：AMD和CMD",
+          "time": "2017-01-01",
+          "content": "简单的说，一个模块就是实现特定功能的文件，有了模块，我们就可以更方便地使用别人的代码，想要什么功能，就加载什么模块。当然，模块开发需要遵循一定的规范，否则各用各的就会乱套了。"
+        }
+      ]
+    }
+  ]
+};
