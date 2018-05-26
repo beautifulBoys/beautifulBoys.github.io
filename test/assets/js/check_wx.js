10c02b56198ff4ae1fead261d0008c76a7e79e31
beautifulBoys <1298947916@qq.com>
2018-05-26 21:10:55 +0800

    a

--- test/assets/js/check_wx.js
+++
@@ -1,5 +0,0 @@
-window.onload = (function() {
-  if (!/micromessenger/.test(navigator.userAgent.toLowerCase())) {
-   window.location.href = 'http://wx.10086.cn/liaoning/global/tiger/welcome_wechat.html#/'
-  }
-}())
