655905c260f037ab2c12ffcf2630cba8d1a483e9
lixin_boy <1298947916@qq.com>
2020-07-15 10:55:02 +0800

    调整图片位置

--- home/data.js
+++ home/data.js
@@ -25,7 +25,7 @@ var data = {
           "title": "AIM 愛聊",
           "time": "2017-10-16",
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a17.jpg",
+          "img": "/source/home/img/a17.jpg",
           "site": {
             "official": "",
             "model": "../pc/vue-socket/index.html"
@@ -36,7 +36,7 @@ var data = {
           "title": "后台管理系统",
           "time": "2017-04-16",
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a16.jpg",
+          "img": "/source/home/img/a16.jpg",
           "site": {
             "official": "",
             "model": "../pc/responsive/index.html"
@@ -47,7 +47,7 @@ var data = {
           "title": "唯一视觉",
           "time": "2016-03-21",
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a1.jpg",
+          "img": "/source/home/img/a1.jpg",
           "site": {
             "official": "http://www.onlyphoto.net.cn/",
             "model": "../pc/weiyi/index.html"
@@ -58,7 +58,7 @@ var data = {
           "title": "小米网",
           "time": "2016-12-08",
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a2.jpg",
+          "img": "/source/home/img/a2.jpg",
           "site": {
             "official": "http://www.mi.com/",
             "model": "../pc/xiaomi/index.html"
@@ -69,7 +69,7 @@ var data = {
           "title": "豆瓣电影",
           "time": "2016-06-15",
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a3.jpg",
+          "img": "/source/home/img/a3.jpg",
           "site": {
             "official": "https://movie.douban.com/",
             "model": "../pc/douban/index.html"
@@ -80,7 +80,7 @@ var data = {
           "title": "天猫登录",
           "time": "2016-07-03",
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a4.jpg",
+          "img": "/source/home/img/a4.jpg",
           "site": {
             "official": "http://login.tmall.com",
             "model": "../pc/tmall/index.html"
@@ -91,7 +91,7 @@ var data = {
           "title": "沈阳化工大学",
           "time": "2016-02-19",
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a11.jpg",
+          "img": "/source/home/img/a11.jpg",
           "site": {
             "official": "http://www.syuct.edu.cn/",
             "model": "../pc/huada/index.html"
@@ -107,51 +107,51 @@ var data = {
       "desc": "移动端流式布局网站",
       "items": [
         {
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a15.jpg",
-          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/tourism.png",
+          "img": "/source/home/img/a15.jpg",
+          "imgma": "/source/home/code/tourism.png",
 		  "url": "https://beautifulboys.github.io/phone/tourism/",
           "title": "旅游圈-手机版",
           "time": "2017-11-10",
           "content": "旅游圈项目的手机版本，启用了全新的手机端设计，与pc端共享账号体系与数据内容。"
         },
         {
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a19.jpg",
-          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/responsive.png",
+          "img": "/source/home/img/a19.jpg",
+          "imgma": "/source/home/code/responsive.png",
           "title": "后台管理系统",
           "time": "2017-04-18",
           "content": "为上方PC端网站增加一个手机浏览入口。"
         },
         {
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a13.jpg",
-          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/question.png",
+          "img": "/source/home/img/a13.jpg",
+          "imgma": "/source/home/code/question.png",
           "title": "答题系统",
           "time": "2017-03-28",
           "content": "该答题系统将答题流程程序化，可以很方便的将题目呈现给别人。前端展示来源于驾校一点通小程序，用VUE做数据驱动，未借助其他工具库。目前只支持单选，以后会陆续实现多功能化。"
         },
         {
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a5.jpg",
-          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/app.png",
+          "img": "/source/home/img/a5.jpg",
+          "imgma": "/source/home/code/app.png",
           "title": "APP随想",
           "time": "2017-03-05",
           "content": "App随想是本人在业余学习过程中的积累，内容繁杂，也许是一个好看的网页，也许是为了研究某种特效或技术而随意而为。"
         },
         {
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a6.jpg",
-          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/mi.png",
+          "img": "/source/home/img/a6.jpg",
+          "imgma": "/source/home/code/mi.png",
           "title": "小米网移动",
           "time": "2017-02-12",
           "content": "本站是小米web官网的仿版。小米官网通体设计为白色背景，显得干净整洁；内容多，却不显杂乱。看到喜欢的页面，就忍不住想要模仿一个…… 本站采用vue整套技术栈。"
         },
         {
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a7.jpg",
-          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/bantang.png",
+          "img": "/source/home/img/a7.jpg",
+          "imgma": "/source/home/code/bantang.png",
           "title": "半糖App",
           "time": "2016-09-20",
           "content": "本项目是模仿半糖App写的。"
         },
         {
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a8.jpg",
-          "imgma": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/code/baifubao.png",
+          "img": "/source/home/img/a8.jpg",
+          "imgma": "/source/home/code/baifubao.png",
           "title": "百付宝",
           "time": "2015-07-10",
           "content": "此项目是根据百度钱包手机APP仿写。包含6个页面：个人中心，账单，充值，亲密付，礼券，礼券单页。"
@@ -166,7 +166,7 @@ var data = {
       "items": [
         {
           "href": "../canvas/time/index.html",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a9.jpg",
+          "img": "/source/home/img/a9.jpg",
           "title": "酷炫时间",
           "time": "2017-02-20",
           "content": "一个本地时间显示的小demo，特点是加了些canvas动画，时间改变的时候，五彩斑斓的小球在页面上坠落弹跳。。。"
@@ -181,21 +181,21 @@ var data = {
       "items": [
         {
           "href": "../example/bullet/index.html",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a13.jpg",
+          "img": "/source/home/img/a13.jpg",
           "title": "弹幕系统",
           "time": "2016-10-10",
           "content": "该弹幕系统是完全模仿哔哩哔哩弹幕网制作。前端用vue，bootstrap，jquery完成功能。通信采用socket.io，后端用NodeJS完成，数据库采用MongoDB。"
         },
         {
           "href": "../example/taobao/index.html",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a14.jpg",
+          "img": "/source/home/img/a14.jpg",
           "title": "淘宝大图浏览",
           "time": "2016-03-26",
           "content": "这是模仿淘宝网商品详情页大图浏览的功能。"
         },
         {
           "href": "../example/resume/index.html",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a15.jpg",
+          "img": "/source/home/img/a15.jpg",
           "title": "个人简历",
           "time": "2016-11-26",
           "content": "个人简历。"
@@ -210,28 +210,28 @@ var data = {
       "items": [
         {
           "href": "../blog/note/index.html",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a1.jpg",
+          "img": "/source/home/img/a1.jpg",
           "title": "JavaScript事件委托",
           "time": "2017-01-19",
           "content": "使用事件委托技术能让你避免对特定的每个节点添加事件监听器；相反，事件监听器是被添加到它们的父元素上。事件监听器会分析从子元素冒泡上来的事件，找到是哪个子元素的事件。基本概念非常简单，但仍有很多人不理解事件委托的工作原理。这里我将要解释事件委托是如何工作的，并提供几个纯JavaScript的基本事件委托的例子。"
         },
         {
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a2.jpg",
+          "img": "/source/home/img/a2.jpg",
           "title": "待填充",
           "time": "2016-01-01",
           "content": "待填充"
         },
         {
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a3.jpg",
+          "img": "/source/home/img/a3.jpg",
           "title": "待填充",
           "time": "2016-01-01",
           "content": "待填充"
         },
         {
           "href": "",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a4.jpg",
+          "img": "/source/home/img/a4.jpg",
           "title": "待填充",
           "time": "2016-01-01",
           "content": "待填充"
@@ -246,42 +246,42 @@ var data = {
       "items": [
         {
           "href": "http://www.cnblogs.com/dushao/p/6004883.html",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a5.jpg",
+          "img": "/source/home/img/a5.jpg",
           "title": "JavaScript十大经典算法",
           "time": "2017-01-01",
           "content": "JavaScript最经典的十种排序算法：冒泡排序，选择排序，插入排序，希尔排序，快速排序，归并排序，堆排序，计数排序，桶排序，基数排序"
         },
         {
           "href": "http://www.codeceo.com/article/javascript-closure-interview.html",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a6.jpg",
+          "img": "/source/home/img/a6.jpg",
           "title": "JavaScript经典闭包题",
           "time": "2017-01-01",
           "content": "这是一道非常典型的JS闭包问题。其中嵌套了三层fun函数，搞清楚每层fun的函数是那个fun函数尤为重要。"
         },
         {
           "href": "http://blog.csdn.net/jeffhtlee/article/details/6830458",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a7.jpg",
+          "img": "/source/home/img/a7.jpg",
           "title": " Javascript的原型继承是如何工作的",
           "time": "2017-01-01",
           "content": "到处可以看到Javascript是原型继承，但Javascript只提供了使用new关键字来实现原型继承，所以很多文章读后有诸多的疑问。这篇文章目的就是要说明Javascript的原型继承是如何工作的。"
         },
         {
           "href": "https://www.talkingcoder.com/article/6404070502094668332",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a8.jpg",
+          "img": "/source/home/img/a8.jpg",
           "title": "JavaScript实现继承的方式",
           "time": "2017-01-01",
           "content": "主要介绍JavaScript实现继承的方式：类式继承、构造函数继承、组合继承、寄生组合式继承、extends继承"
         },
         {
           "href": "http://ghmagical.com/article/page/id/UPLfoGI9vJ91",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a12.jpg",
+          "img": "/source/home/img/a12.jpg",
           "title": "JS中的call、apply、bind方法",
           "time": "2017-01-01",
           "content": "在JavaScript中，call、apply和bind 是Function对象自带的三个方法，这三个方法的主要作用是改变函数中的this指向。"
         },
         {
           "href": "http://ghmagical.com/article/page/id/N7VY7Hg4TlgW",
-          "img": "https://raw.githubusercontent.com/beautifulBoys/beautifulBoys.github.io/master/source/home/img/a13.jpg",
+          "img": "/source/home/img/a13.jpg",
           "title": "浅析JS模块规范：AMD和CMD",
           "time": "2017-01-01",
           "content": "简单的说，一个模块就是实现特定功能的文件，有了模块，我们就可以更方便地使用别人的代码，想要什么功能，就加载什么模块。当然，模块开发需要遵循一定的规范，否则各用各的就会乱套了。"
